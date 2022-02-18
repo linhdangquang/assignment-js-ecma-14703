2f8d6cf0a36a86667756f59466bc997022d43f1d
DLinh <nogamenolife.why@gmail.com>
2022-02-18 21:35:59 +0700

    feat: sort categories lodash

--- src/utils/sort.js
+++ src/utils/sort.js
@@ -1,19 +1,23 @@
+import orderBy from 'lodash.orderby';
+
 export const SortCategoriesAsc = (array) => {
-  array.sort((a, b) => {
-    const keyA = a.products.length;
-    const keyB = b.products.length;
-    if (keyA < keyB) return -1;
-    if (keyA > keyB) return 1;
-    return 0;
-  });
+  orderBy(array, ['products.length'], ['asc']);
+  // array.sort((a, b) => {
+  //   const keyA = a.products.length;
+  //   const keyB = b.products.length;
+  //   if (keyA < keyB) return -1;
+  //   if (keyA > keyB) return 1;
+  //   return 0;
+  // });
 };
 
 export const SortCategoriesDesc = (array) => {
-  array.sort((a, b) => {
-    const keyA = a.products.length;
-    const keyB = b.products.length;
-    if (keyA > keyB) return -1;
-    if (keyA < keyB) return 1;
-    return 0;
-  });
+  orderBy(array, ['products.length'], ['desc']);
+  // array.sort((a, b) => {
+  //   const keyA = a.products.length;
+  //   const keyB = b.products.length;
+  //   if (keyA > keyB) return -1;
+  //   if (keyA < keyB) return 1;
+  //   return 0;
+  // });
 };
